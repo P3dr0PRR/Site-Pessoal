511663476387da43a778fcbeabce8def5d2a520a
Pedro <pedropaulo@interlinksistemas.com.br>
2026-01-26 15:32:47 -0300

    feet: Att do link ao portifolio

--- src/Pages/pageMeetMe/Pages/linkArea/LinksTo.jsx
+++ src/Pages/pageMeetMe/Pages/linkArea/LinksTo.jsx
@@ -9,7 +9,7 @@ export function LinksTo({ isDark }) {
         target="_blank"
         rel="noopener noreferrer"
         className={`rounded-lg p-2 border-2 ${isDark ? "bg-gray-800 border-gray-800 text-gray-100 hover:bg-blue-800/80 hover:border-blue-500" : "bg-sky-200/80 hover:bg-sky-300 hover:border-sky-400"} text-center hover:scale-105 transition-transform duration-300`}
-        href="https://portif-i97470w6k-pedros-projects-dd604c73.vercel.app/"
+        href="https://portif-4wn5ianem-pedros-projects-dd604c73.vercel.app/"
       >
         Ver meu Portifolio
       </a>
